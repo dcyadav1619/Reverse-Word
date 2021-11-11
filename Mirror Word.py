2944854558cb2459a84976c2927b5aa750364a3d
DINESH  CHANDRA <90493714+dcyadav1619@users.noreply.github.com>
2021-11-11 18:22:29 +0530

    Add files via upload

---
+++ Mirror Word.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program that accepts a word from the user and reverse it.
+
+# In[3]:
+
+
+word = input("enter the word")
+
+print(word[::-1])
+
